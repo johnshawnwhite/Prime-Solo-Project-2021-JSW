53ea2674a55283c03865acaa28a54637a8c5dae0
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-08-09 14:52:43 -0400

    small change

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -8,6 +8,7 @@ function UserPage() {
   return (
     <div className="container">
       <h2>You've made it, {user.username}!</h2>
+      <h3>Feel free to look at our list of mountains so that you can place some information on a map for later!</h3>
       {/* <p>Your ID is: {user.id}</p> */}
       <LogOutButton className="btn" />
     </div>

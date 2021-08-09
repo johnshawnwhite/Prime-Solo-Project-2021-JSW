d5e40676fe167a7da9fb0fb91cf1fe3dc370117a
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-08-09 14:52:25 -0400

    closer to correct

--- src/components/Mountains/Mountain.jsx
+++ src/components/Mountains/Mountain.jsx
@@ -3,36 +3,57 @@ import React from 'react';
 
 
 function Mountains() {
+    // const onSelectOverlay = (OverlayId) => {
+    //     history.push(`/overlays/${OverlayId}`);
+
   return (
     <div className="container">
       <div>
         <p>Here are some possibilties for your favorite spot!</p>
-        <button>Snoqulmie</button>
-// click event will take you to the location of summit at snoqualmie
-        <p></p><button>Mt Baker</button>
-        // click event will take you to the location of Mt Baker
-        <p></p><button>Mt Hood</button>
-        // click event will take you to the location of Mt hood
-        <p></p><button>Stevens pass</button>
-        // click event will take you to the location of Stevens Pass
+        {/* <p></p><button onClick={`/overlays/${OverlayId}`}>Snoqulmie</button> */}
+            // click event will take you to the location of summit at snoqualmie
+        {/* <p></p><button onClick={`/overlays/${OverlayId}`}>Mt Baker</button> */}
+            // click event will take you to the location of Mt Baker
+        {/* <p></p><button onClick={`/overlays/${OverlayId}`}>Mt Hood</button> */}
+            // click event will take you to the location of Mt hood
+        {/* <p></p><button onClick={`/overlays/${OverlayId}`}>Stevens pass</button> */}
+            // click event will take you to the location of Stevens Pass
       </div>
       {/* <div ref = {this.mapContainer} className = "mapContainer"/> */}
-      
-      <script>
-
-{/*  
-var map = new mapboxgl.Map({
-container: 'map',
-style: '
-mapbox://styles/mapbox/streets-v11',
-center: [-121.426, 47.44],
-zoom: 13
-}); */}
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmlnYWlyam9uc29uIiwiYSI6ImNrcnM3MmVtazM0ODUyd2tkdXg5bGJsZ2EifQ.1idatnzmiZ46CAl2KvCWvQ';
-</script>
       </div>
   )
 }
 
 
 export default Mountains;
+// import * as React from 'react';
+// import {useState} from 'react';
+// import {render} from 'react-dom';
+// import MapGL from 'react-map-gl';
+
+// const MAPBOX_TOKEN = ''; // Set your mapbox token here
+
+// function Root() {
+//   const [viewport, setViewport] = useState({
+//     latitude: 37.8,
+//     longitude: -122.4,
+//     zoom: 14,
+//     bearing: 0,
+//     pitch: 0
+//   });
+
+//   return (
+//     <MapGL
+//       {...viewport}
+//       width="100vw"
+//       height="100vh"
+//       mapStyle="mapbox://styles/mapbox/dark-v9"
+//       onViewportChange={setViewport}
+//       mapboxApiAccessToken={MAPBOX_TOKEN}
+//     />
+//   );
+// }
+
+// document.body.style.margin = 0;
+// render(<Root />, document.body.appendChild(document.createElement('div')));
+

12b54c95ba94874107441304436bae41f34024d5
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-08-11 00:27:46 -0400

    reducers

--- src/redux/reducers/location.reducer.js
+++ src/redux/reducers/location.reducer.js
@@ -1,12 +1,29 @@
+import { combineReducers } from "redux";
 
 const mountainLocations = (state = [], action) => {
-    switch (action.type) {
-        case 'SET_Mountain_LOCATIONS':
-          console.log('list of mountain coordinates',action.payload);
-          return state = action.payload.data
-        default:
-          return state;
-      }
-}
+  switch (action.type) {
+    case "SET_MOUNTAIN_LOCATIONS":
+      console.log("list of mountain coordinates", action.payload);
+      return action.payload.data;
+    default:
+      return state;
+  }
+};
 
-export default mountainLocations;
+const overlayStartingPoints = (
+  state = { longitude: -121.437, latitude: 47.44 },
+  action
+) => {
+  switch (action.type) {
+    case "SET_OVERLAY_STARTING_POINTS":
+      console.log("list of mountain coordinates", action.payload);
+      return action.payload;
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({
+  mountainLocations,
+  overlayStartingPoints,
+});

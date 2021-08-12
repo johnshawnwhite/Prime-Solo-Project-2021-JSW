4d9f4395187b246dc371f70ea5c037a4df37e5f2
johnshawnwhite <82325558+johnshawnwhite@users.noreply.github.com>
2021-08-12 19:10:01 -0400

    moved marker

--- src/redux/sagas/location.saga.js
+++ src/redux/sagas/location.saga.js
@@ -1,71 +1,62 @@
-import { put, takeEvery } from '@redux-saga/core/effects';
-import axios from 'axios';
-function* locationSaga () {
-    
-    yield takeEvery('SET_MOUNTAIN_COORDINATES', getOverlayCoordinates);
-    yield takeEvery('ADD_MARKER', addMarker);
-    yield takeEvery('GET_MARKER_DATA', deleteMarker);
-    yield takeEvery('GET_MARKER', findMarker);
-    yield takeEvery('CHANGE_MARKER_DATA', updateMarker);
-    }
+import { put, takeEvery } from "@redux-saga/core/effects";
+import axios from "axios";
+function* locationSaga() {
+  yield takeEvery("SET_MOUNTAIN_COORDINATES", getOverlayCoordinates);
+  yield takeEvery("ADD_MARKER", addMarker);
+  yield takeEvery("GET_MARKER_DATA", deleteMarker);
+  yield takeEvery("GET_MARKER", findMarker);
+  yield takeEvery("CHANGE_MARKER_DATA", updateMarker);
+}
 
-    //used to go from the mountains page to the overlays page, directly to the mountain of your choice
+//used to go from the mountains page to the overlays page, directly to the mountain of your choice
 function* getOverlayCoordinates() {
-    try{
-        const mountainList =
-        yield axios.get('api/mountain/coordinates');
-        console.log('where this is', mountainList.data);
-        yield put({type: 'SET_MOUNTAIN_COORDINATES', payload: mountainList})
-    }
-    catch(error) {
-        console.log('problem getting mountain coordinates', error);
-    }
+  try {
+    const mountainList = yield axios.get("api/mountain/coordinates");
+    console.log("where this is", mountainList.data);
+    yield put({ type: "SET_MOUNTAIN_COORDINATES", payload: mountainList });
+  } catch (error) {
+    console.log("problem getting mountain coordinates", error);
+  }
 }
 
 function* addMarker(action) {
-    try{
-        yield axios.post( '/api/coordinates/pointsfeatures/', action.payload);
-        console.log(action.payload);
-      }
-      catch(error) {
-        console.log('Error adding Marker', error);
-      }
-  }
-
-
-function* findMarker(){
-    try{
-    const response = yield axios.get('/api/coordinates/pointsfeatures/');
-    yield put({
-        type: 'GET_MARKERS',
-        payload: response.data
-    })
-    } 
-    catch(err) {
-        console.log('Error GETing favorite', err);
-    }
-}
-
-
-  function* deleteMarker(action){
+  try {
+    yield axios.post("/api/coordinates/pointsfeatures/", action.payload);
     console.log(action.payload);
-    try{
-    yield axios.delete(`/api/overlays/marker/${action.payload.id}`);
-    yield put({type: 'GET_MARKER_DATA'});
-    } 
-    catch(err) {
-        console.log('Error deleting marker', err);
-    }
+  } catch (error) {
+    console.log("Error adding Marker", error);
+  }
 }
-  
-  function* updateMarker(action){
-    try{
+
+function* findMarker() {
+  try {
+    const response = yield axios.get("/api/coordinates/pointsfeatures/");
+    yield put({
+      type: "GET_MARKERS",
+      payload: response.data,
+    });
+  } catch (err) {
+    console.log("Error GETing favorite", err);
+  }
+}
+
+function* deleteMarker(action) {
+  console.log(action.payload);
+  try {
+    yield axios.delete(`/api/overlays/marker/${action.payload.id}`);
+    yield put({ type: "GET_MARKER_DATA" });
+  } catch (err) {
+    console.log("Error deleting marker", err);
+  }
+}
+
+function* updateMarker(action) {
+  try {
     yield axios.put(`/api/overlays/marker/${action.payload}`);
-    yield put({type: 'CHANGE_MARKER_DATA'});
-    } 
-    catch(err) {
-        console.log('Error Finding Marker DATA', err);
-    }
+    yield put({ type: "CHANGE_MARKER_DATA" });
+  } catch (err) {
+    console.log("Error Finding Marker DATA", err);
+  }
 }
 
 export default locationSaga;
